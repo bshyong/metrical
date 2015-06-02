9fd978ac55241b645f72c185476e71d3e3767764
Ben <hello@benshyong.com>
2015-06-01 18:23:45 -0700

    annotations"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def index
+    annotations = MIXPANEL_CLIENT.request(
+      '/retention',
+      from_date: '2013-12-1',
+      to_date: '2015-6-1'
+    )
 
-    render text: "hi"
+    render json: annotations
   end
 end
